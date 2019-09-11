2a449c87996915d15268cb23dbc1e258b6dd0557
rdehnhardt <renatotkd@gmail.com>
2019-09-11 10:45:22 +0200

    Artisan publish

--- src/SkeletonServiceProvider.php
+++ src/SkeletonServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Skeleton;
 
+use Artisan;
 use Illuminate\Support\ServiceProvider;
 
 class SkeletonServiceProvider extends ServiceProvider
@@ -15,5 +16,10 @@ class SkeletonServiceProvider extends ServiceProvider
 
         $this->publishes([__DIR__ . '/../resources/compiled/css/skeleton.css' => public_path('skeleton/css/app.css')], 'skeleton');
         $this->publishes([__DIR__ . '/../resources/compiled/js/skeleton.js' => public_path('skeleton/js/app.js')], 'skeleton');
+    }
+
+    protected function publishAssets()
+    {
+        Artisan::call('vendor:publish', ['--tag' => 'skeleton', '--force' => true]);
     }
 }
